a2d6d5f4d52686c874b7fde1e91502a3a59136ab
brudko <brudko@bivgroup.ru>
2024-05-03 18:37:10 +0300

    implements Persistable

--- src/main/java/com/ersted/userservices/entity/Country.java
+++ src/main/java/com/ersted/userservices/entity/Country.java
@@ -4,14 +4,16 @@ import lombok.Data;
 import lombok.ToString;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.annotation.Transient;
+import org.springframework.data.domain.Persistable;
 import org.springframework.data.relational.core.mapping.Table;
+import org.springframework.util.StringUtils;
 
 import java.time.LocalDateTime;
 import java.util.Set;
 
 @Data
 @Table("countries")
-public class Country {
+public class Country implements Persistable<String> {
     @Id
     private String id;
     private LocalDateTime created;
@@ -24,4 +26,9 @@ public class Country {
     @Transient
     @ToString.Exclude
     private Set<Address> addresses;
+
+    @Override
+    public boolean isNew() {
+        return !StringUtils.hasText(id);
+    }
 }
