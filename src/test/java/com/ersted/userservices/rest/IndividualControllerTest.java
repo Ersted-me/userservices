6c2062400740147827e13b38e6a2b26722b2fd3e
brudko <brudko@bivgroup.ru>
2024-05-06 21:06:36 +0300

    add tests for individual controller

---
+++ src/test/java/com/ersted/userservices/rest/IndividualControllerTest.java
@@ -0,0 +1,75 @@
+package com.ersted.userservices.rest;
+
+import com.ersted.userservices.exception.BadRequestException;
+import com.ersted.userservices.service.IndividualService;
+import com.ersted.userservices.utils.IndividualDataUtils;
+import com.ersted.userservices.utils.ResponseDataUtils;
+import net.ersted.dto.IndividualDto;
+import net.ersted.dto.ResponseDto;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.BDDMockito;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Mono;
+
+import static org.mockito.ArgumentMatchers.any;
+
+@ExtendWith(SpringExtension.class)
+@WebFluxTest(controllers = {IndividualController.class})
+class IndividualControllerTest {
+
+    @Autowired
+    private WebTestClient webTestClient;
+    @MockBean
+    private IndividualService individualService;
+
+    @Test
+    @DisplayName("registration individual")
+    public void givenIndividualDto_whenRegistration_thenResponseDtoIsReturned() {
+        //given
+        IndividualDto individualDto = IndividualDataUtils.individualDto();
+        ResponseDto successResponse = ResponseDataUtils.success("createdId", "Individual has been successfully registered");
+        BDDMockito.given(individualService.registration(any(IndividualDto.class)))
+                .willReturn(Mono.just(successResponse));
+        //when
+        WebTestClient.ResponseSpec response = webTestClient.post()
+                .uri("/api/v1/individuals")
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(Mono.just(individualDto), IndividualDto.class)
+                .exchange();
+        //then
+        response.expectStatus().isOk()
+                .expectBody()
+                .jsonPath("$.id").isEqualTo("createdId")
+                .jsonPath("$.message").isEqualTo("Individual has been successfully registered")
+                .jsonPath("$.status").isEqualTo("SUCCESS");
+    }
+
+    @Test
+    @DisplayName("registration nullable individual")
+    public void givenNullableIndividualDto_whenRegistration_thenBadRequestIsReturned() {
+        //given
+
+        BDDMockito.given(individualService.registration(any(IndividualDto.class)))
+                .willReturn(Mono.error(new BadRequestException("BAD_REQUEST", "Body can not be blank")));
+        //when
+        WebTestClient.ResponseSpec response = webTestClient.post()
+                .uri("/api/v1/individuals")
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(Mono.empty(), IndividualDto.class)
+                .exchange();
+        //then
+        response.expectStatus().isOk()
+                .expectBody()
+                .jsonPath("$.id").isEqualTo("createdId")
+                .jsonPath("$.message").isEqualTo("Individual has been successfully registered")
+                .jsonPath("$.status").isEqualTo("SUCCESS");
+    }
+}
---
+++ src/test/java/com/ersted/userservices/utils/ResponseDataUtils.java
@@ -0,0 +1,14 @@
+package com.ersted.userservices.utils;
+
+import com.ersted.userservices.enums.ResponseStatus;
+import net.ersted.dto.ResponseDto;
+
+public class ResponseDataUtils {
+    public static ResponseDto success(String id, String message){
+        return new ResponseDto(ResponseStatus.SUCCESS.name(), message, id);
+    }
+
+    public static ResponseDto failed(String id, String message){
+        return new ResponseDto(ResponseStatus.FAILED.name(), message, id);
+    }
+}
