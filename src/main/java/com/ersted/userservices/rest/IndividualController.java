6acf219d930d0fe53fc80cd9a032740fa568ebef
brudko <brudko@bivgroup.ru>
2024-05-06 21:06:26 +0300

    add individual controller

---
+++ src/main/java/com/ersted/userservices/rest/IndividualController.java
@@ -0,0 +1,24 @@
+package com.ersted.userservices.rest;
+
+import com.ersted.userservices.enums.ResponseStatus;
+import com.ersted.userservices.service.IndividualService;
+import lombok.RequiredArgsConstructor;
+import net.ersted.dto.IndividualDto;
+import net.ersted.dto.ResponseDto;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/individuals")
+public class IndividualController {
+    private final IndividualService individualService;
+
+    @PostMapping
+    public Mono<?> registration(@RequestBody IndividualDto dto) {
+        return individualService.registration(dto);
+    }
+}
